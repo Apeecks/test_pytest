09fdd18ad41232829fb65bf4da7accbe063fb92a
Apeecks <apeecks@Ubuntu.dlink>
2025-02-11 11:25:43 +0300

    fixes test and calculate_taxes

--- src/calculate_taxes.py
+++ src/calculate_taxes.py
@@ -1,15 +1,32 @@
-def calculate_taxes(prices: list[float], tax_rate: float) -> list[float]:
+from typing import Union
+
+
+def calculate_taxes(prices: list[float], tax_rate: float, discount: Union[int, float]=0) -> list[float]:
     """Функция вычисляет стоимость товаров с учётом налога."""
 
     if tax_rate < 0:
         raise ValueError('Неверный налоговый процент')
+    elif tax_rate == prices[0]:
+        raise ValueError('Неверный налоговый процент')
+
+    if type(discount) != int:
+        if type(discount) != float:
+            raise TypeError('Ошибка типа данных')
 
     taxed_prices = []
+    taxed_prices_discount_list = []
 
     for price in prices:
         if price <= 0:
             raise ValueError('Неверная цена')
         tax = price * tax_rate / 100
-        taxed_prices.append(price + tax)
+        taxed_prices.append(tax + price)
 
-    return taxed_prices
+
+    if discount == 0:
+        return taxed_prices
+    else:
+        for taxed_price in taxed_prices:
+            taxed_prices_discount = taxed_price - (taxed_price / 100 * discount)
+            taxed_prices_discount_list.append(round(taxed_prices_discount, 2))
+        return taxed_prices_discount_list
--- tests/test_calculate_taxes.py
+++ tests/test_calculate_taxes.py
@@ -8,10 +8,12 @@ def test_calculate_taxes_zero(value_zero_prices):
         calculate_taxes(value_zero_prices[0], value_zero_prices[1])
     assert str(file.value) == 'Неверная цена'
 
+
 def test_calculate_taxes_zero_rate(value_zero_rate):
     with pytest.raises(ValueError) as file:
         calculate_taxes(value_zero_rate[0], value_zero_rate[1])
     assert str(file.value) == 'Неверный налоговый процент'
+
 
 @pytest.mark.parametrize ("prices, tax_rate, result", [
     ([100], 1.0, [101]),
@@ -20,3 +22,24 @@ def test_calculate_taxes_zero_rate(value_zero_rate):
 ])
 def test_calculate_taxes_parametrise(prices, tax_rate, result):
     assert calculate_taxes(prices, tax_rate) == result
+
+
+def test_calculate_taxes_prices_tax_error():
+    with pytest.raises(ValueError) as file:
+        calculate_taxes([100], 100)
+    assert str(file.value) == 'Неверный налоговый процент'
+
+
+@pytest.mark.parametrize ("prices, tax_rate, discount, result", [
+    ([100], 2.0, 10, [91.80]),
+    ([950], 3.0, 7.0, [910.00]),
+    ([111], 7.0, 13, [103.33])
+])
+def test_calculate_taxes_discount(prices, tax_rate, discount, result):
+    assert calculate_taxes(prices, tax_rate, discount) == result
+
+
+def test_calculate_taxes_error_type():
+    with pytest.raises(TypeError) as file:
+        calculate_taxes([100], 1.0, "Привет")
+    assert str(file.value) == 'Ошибка типа данных'
